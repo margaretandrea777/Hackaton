58e63956a8753680445bb1ac34dcfea5f7e1e5fc
Sara1812 <sarabea18@gmail.com>
2019-05-18 13:09:18 -0400

    nanana

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 app=Flask(__name__)
-
 @app.route("/platahetaap")
 def render_html():
-    return render_template("Primeracarilla.html")
+    return render_template("index.html")
