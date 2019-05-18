008c255f7a9bc417bdc98955e182d120619c4332
fanegol <fanego27@hotmail.com>
2019-05-18 16:54:26 -0400

    se agrego para leer el post

--- app.py
+++ app.py
@@ -1,9 +1,46 @@
 from flask import Flask, render_template
 app=Flask(__name__)
+
 @app.route("/")
 def render_html():
     return render_template("bienvenida.html")
 
-@app.route("/index.html")
+@app.route("/index")
 def render_index():
     return render_template("index.html")
+
+
+@app.route("/registro", methods)
+def render_index():
+    return render_template("index.html")
+
+@app.route('/registro2', methods=['POST','GET'])      # aca es para registrar al animal
+def registro():
+    if request.method=='POST':   
+        try:
+            ci=request.form['ci']                             #identificativo del animal
+            datos=[ci]  # esto es para meter en la db luego
+            with sql.connect(nombre_db) as con:        
+                cur = con.cursor()
+                cur.execute('''CREATE TABLE IF NOT EXISTS usuario (
+                                        ci integer ,
+                                        NumeroT interger,                                        
+                                        Gastos integer,
+                                        ,
+                                        Ultimafecha text,
+                                        Vacunas text
+                                    );'''
+                       )
+                cur.execute('''INSERT INTO usuario (ci) VALUES (?,);''', datos )
+            
+                con.commit()   
+             
+        except:
+            con.rollback()
+
+        finally:
+            con.close()# cerramos la conexion de la base de datos 
+            js=lista()   #retornamos datos de la db para el form del lado del cliente
+            return render_template('registro.html',dato=js)
+            
+
