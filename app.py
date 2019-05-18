bcb57060d067c018f70795388f92accd48e38387
Sara1812 <sarabea18@gmail.com>
2019-05-18 16:53:54 -0400

    ñandejara

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ app=Flask(__name__)
 def render_html():
     return render_template("bienvenida.html")
 
-@app.route("/index.html")
+@app.route("/2da.html")
 def render_index():
-    return render_template("index.html")
+    return render_template("2da.html")
+
+@app.route("/3ra.html")
+def render_inde():
+    return render_template("3ra.html")
