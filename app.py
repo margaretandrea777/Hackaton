f991ca49dd15509cb18685998d62268a9e8ae114
Sara1812 <sarabea18@gmail.com>
2019-05-18 15:37:42 -0400

    nananananan

--- app.py
+++ app.py
@@ -2,4 +2,8 @@ from flask import Flask, render_template
 app=Flask(__name__)
 @app.route("/")
 def render_html():
+    return render_template("bienvenida.html")
+
+@app.route("/index.html")
+def render_index():
     return render_template("index.html")
